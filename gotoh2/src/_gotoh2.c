db653ea7de79eb946de20e35ce4bfbf9da6480ed
Art Poon <artpoon@gmail.com>
2016-08-02 00:07:44 -0700

    I think sequence encoding is working, now starting to implement matrix computation

--- gotoh2/src/_gotoh2.c
+++ gotoh2/src/_gotoh2.c
@@ -28,31 +28,81 @@ void map_ascii_to_alphabet (int * map, const char * alphabet) {
     }
 }
 
-int * encode_sequence (const char * seq, int * map) {
+void encode_sequence (const char * seq, int * map, int * encoded) {
     int seqlen = (int) strlen(seq);
-    int * encoded[seqlen];
     for (int i=0; i < seqlen; i++) {
         encoded[i] = map[(int)seq[i]];
     }
 }
 
 
+void populate_D_matrix(int * d, int nrows, int ncols, struct align_settings s) {
+    int max_dim = nrows;
+    if (nrows < ncols) {
+        max_dim = ncols;
+    }
+
+    // D(0,0) = 0
+    d[0] = 0;
+
+    // linearize cache matrices so that (m,n) = (m * ncols + n)
+    int p[nrows*ncols];
+    int q[nrows*ncols];
+
+    // initialize left column
+    for (int m = 0; m < nrows; m++) {
+        d[m*ncols] = p[m*ncols] = s.is_global ? (s.v+s.u*m) : 0;
+    }
+
+    // initialize the top row
+    for (int n = 0; n < ncols; n++) {
+        d[n] = q[n] = s.is_global ? (s.v+s.u*n) : 0;
+    }
+
+    // iterate through D matrix by diagonals
+    // http://stackoverflow.com/questions/1779199/traverse-matrix-in-diagonal-strips
+    for (int z, slice=0; slice<(2*max_dim-1); slice++) {
+        z = (slice < max_dim) ? 0 : (slice-max_dim+1);  // which side are we on?
+        for (int j=z; j<=slice-z; )
+    }
+
+    for (int m = 1; m < nrows; m++) {
+        for (int n = 1; n < ncols; n++) {
+            d[m*ncols+n] = 0;
+        }
+    }
+
+}
+
 
 // main wrapper function
 struct align_output align(const char * seq1, const char * seq2, struct align_settings m) {
     int map[256] = {0};
+    int l1 = (int) strlen(seq1);
+    int l2 = (int) strlen(seq2);
+    int sA[l1];
+    int sB[l2];
+
+    int d[(l1+1)*(l2+1)];
     struct align_output o;
 
     // 1. convert sequences into integer indices into alphabet
     map_ascii_to_alphabet(map, m.alphabet);
 
-    int * sA = encode_sequence(seq1, map);
-    int * sB = encode_sequence(seq2, map);
+    encode_sequence(seq1, map, sA);
+    encode_sequence(seq2, map, sB);
 
     // 2. generate D matrix
+    populate_D_matrix(d, l1+1, l2+1, m);
 
+    for (int i=0; i<l1+1; i++) {
+        for (int j=0; j<l2+1; j++) {
+            fprintf(stdout, "%d ", d[i*(l2+1)+j]);
+        }
+        fprintf(stdout, "\n");
+    }
 
-    // X. decode aligned integer sequences into alphabet sequences
+    // TODO: decode aligned integer sequences into alphabet sequences
     o.aligned_seq1 = seq1;
     o.aligned_seq2 = seq2;
     o.alignment_score = 0;
