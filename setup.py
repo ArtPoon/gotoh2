e0cdf33a591125c96321dd04edec78b852a43681
Art Poon <artpoon@gmail.com>
2020-04-10 12:19:06 -0400

    fixed bug in setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, Extension
-#import numpy as np
 
 setup (name = 'gotoh2',
        version = '0.1',
-       description = "C implementation of Gotoh pairwise alignment algorithm to be wrapped in Python",
-       py_modules = ['gotoh2', 'utils'],
+       description = "C implementation of Gotoh pairwise alignment algorithm to "
+                     "be wrapped in Python",
+       py_modules = ['gotoh2', 'gotoh2_utils'],
        ext_modules = [Extension('_gotoh2', sources = ['src/_gotoh2.c'])],
        data_files = [('models/', [
            'models/HYPHY_NUC.csv',
@@ -13,6 +13,6 @@ setup (name = 'gotoh2',
            'models/EmpHIV25.csv',
            'models/BLOSUM62.csv'
        ])],
-       #include_dirs = [np.get_include()],
-       zip_safe = False  #avoid headache with permissions on ~/.python-eggs
+       # include_dirs = [np.get_include()],
+       zip_safe = False  # avoid headache with permissions on ~/.python-eggs
 )
